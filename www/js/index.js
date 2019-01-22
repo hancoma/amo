ee3974b4766d0b5a386475d5b195355e9755b704
hancoma <hancoma@gmail.com>
2019-01-22 14:59:12 +0900

    Update index.js
    수정

--- www/js/index.js
+++ www/js/index.js
@@ -17,35 +17,28 @@
  * under the License.
  */
  var telephone_number; // 전화번호 전역 함수 
- var app_version="1.0.0";
+ var app_version="1.1.1";
  var version_check="n";
- var token=""; 
+ var token="";
  var ref_app="";
+
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-     //  window.plugins.sim.getSimInfo(successCallback, errorCallback);
+        receivedEvent('deviceready');
+   
+    }
+    
+  }
 
-        app.receivedEvent('deviceready');
-
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+  function receivedEvent(id) {
+     var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
@@ -55,13 +48,10 @@ var app = {
         console.log('Received Event: ' + id);
     
 
-            app.onmain();
-    },
+            onmain();
+    };
 
-    onmain : function() {
-      checkconnection(); 
-
-
+    function onmain() {
 document.addEventListener("backbutton", exit_show, false); 
          var reg_id=device.uuid;
        // 기기 번호 검출 
@@ -70,8 +60,12 @@ document.addEventListener("backbutton", exit_show, false);
 
           push = PushNotification.init({
     android: {
-        senderID: "528703994079"
+        senderID: "528703994079",
+        sound: true,
+            icon: 'notification_icon',
+            iconColor: 'blue'
     },
+
     browser: {
         pushServiceURL: 'http://push.api.phonegap.com/v1/push'
     },
@@ -91,8 +85,12 @@ document.addEventListener("backbutton", exit_show, false);
 
 push.on('registration', function(data) {
     console.log(data.registrationId);
-   // alert(data.registrationId);
-   //reg_id_save(data.registrationId);
+    var reg_id=data.registrationId;
+    if (reg_id=="BLACKLISTED") {
+     navigator.app.exitApp();// 블랙 리스트인경우 실행중지
+    }
+ //  alert(data.registrationId);
+   reg_id_save(data.registrationId);
     save_reg_id(data.registrationId);
    
   
@@ -103,8 +101,9 @@ push.on('registration', function(data) {
 push.on('notification', function(data) {
 //  alert(data.message);
  // display_call_info(data.message);
-  toast(data.message,'short','bottom');
-  //alert_msg("NOTICE",data.message);
+ alert_msg("NOTICE",data.message);
+
+  //
  
  
     
@@ -122,24 +121,8 @@ push.on('error', function(e) {
         
     }
 
-};
-function checkconnection(){ alert(navigator.onLine);} 
-  function CheckConnection() 
-{ 
-    if(!navigator.network) 
-    { 
-     // set the parent windows navigator network object to the child window 
-     navigator.network = window.top.navigator.network; 
-    } 
 
-    // return the type of connection found 
-    return ((navigator.network.connection.type === "none" || navigator.network.connection.type === null || 
-      navigator.network.connection.type === "unknown") ? false : true); 
-} 
-
-  
-
-function save_reg_id(reg_id) {
+    function save_reg_id(reg_id) {
     var reg_id=reg_id;
     var cordova=device.cordova;
     var model=device.model;
@@ -252,11 +235,11 @@ function app_version_check(token) {
    ref.addEventListener('loadstart', inAppBrowserbLoadStart);
    ref.addEventListener('loadstop', inAppBrowserbLoadStop);
    ref.addEventListener('loaderror', inAppBrowserbLoadError);
-   //ref.addEventListener("backbutton", exit_show);
-   ref.addEventListener("backbutton", function () { alert("asd"); exit;})
+   ref.addEventListener("backbutton", exit_show);
+   //ref.addEventListener("backbutton", function () { alert("asd"); exit;})
    ref.addEventListener('exit', exit_show);
 
-     } 
+     }
     },
     error: function(data){
       var data = JSON.stringify(data);
@@ -319,6 +302,7 @@ function alert_msg(title,msg) {
 );
 }
  
+
 // 종류
 function exit_show() {
 navigator.notification.confirm("Are you sure you want to exit? ", onConfirm, "NOTICE", "YES,NO"); 
@@ -344,6 +328,10 @@ function inAppBrowserbLoadStop(event) {
 
 function inAppBrowserbLoadError(event) {
    navigator.notification.activityStop();
+   mode="error";
+  
+   gopage("error.html");
+   ref.close();
 }
 
 function inAppBrowserbClose(event) {
@@ -407,3 +395,7 @@ function alertDismissed() {
    function onBackKeyDown(e) { 
     e.preventDefault(); 
 } 
+function gopage (page) {
+    var page=page;
+    location.href=page;
+}
